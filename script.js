0a7e0bbe19dc2e1c4407dc8548bd3b1a05e2c9dd
irfan <85334710+irfan-007@users.noreply.github.com>
2024-07-12 20:07:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 function fibonacci(num) {
 // your code here
+	let a=0
+	let b=1
+	let ct=3
 	if(num==1)
 		return 0
 	if(num==2)
 		return 1
-	return fibonacci(num-1)+fibonacci(num-2)
+	while(true){
+		let c=a+b
+		if(ct==num)
+			return c
+		a=b
+		b=c
+		ct++
+	}
 }
 
 module.exports = fibonacci;
